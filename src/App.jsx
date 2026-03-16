fe8d1f96dfdf644209f18e639f7c00544f0106c6
Divyanshu Shah <148536322+Divyanshu5009@users.noreply.github.com>
2026-03-16 19:42:13 +0530

    mern-frontend

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import Register from "./components/Register";
+import Cart from "./components/Cart";
+import Orders from "./components/Orders";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createContext, useState } from "react";
+export const AppContext = createContext();
+function App() {
+  const [user, setUser] = useState({});
+  return (
+    <AppContext.Provider value={{ user, setUser }}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index element={<Content />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="logout" element={<Logout />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </AppContext.Provider>
+  );
+}
+export default App;
